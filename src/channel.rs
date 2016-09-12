3edd96a5a358708b6a6f58a4e49d0ed9d6f4a0bf
RT <k.teza1@gmail.com>
2016-09-13 01:17:21 +0530

    adding sychronous channel implementation

--- src/channel.rs
+++ src/channel.rs
@@ -24,6 +24,18 @@ pub struct Sender<T> {
     tx: channel::Sender<T>,
 }
 
+/// The transmission half of a synchronous channel used for sending messages to a receiver.
+///
+/// A `SyncSender` can be `clone`d to have multiple threads or instances sending
+/// messages to one receiver.
+///
+/// This type is created by the [`sync_channel`] function.
+///
+/// [`sync_channel`]: fn.sync_channel.html
+pub struct SyncSender<T> {
+    tx: channel::SyncSender<T>,
+}
+
 /// The receiving half of a channel used for processing messages sent by a
 /// `Sender`.
 ///
@@ -58,6 +70,26 @@ pub fn channel<T>(handle: &Handle) -> io::Result<(Sender<T>, Receiver<T>)>
     Ok((Sender { tx: tx }, Receiver { rx: rx }))
 }
 
+/// Creates a new in-memory bounded channel used for sending data across `Send +
+/// 'static` boundaries, frequently threads.
+///
+/// This type can be used to conveniently send messages between futures.
+/// Unlike the futures crate `channel` method and types, the returned tx/rx
+/// pair is a multi-producer single-consumer (mpsc) channel *with no
+/// backpressure*. Currently it's left up to the application to implement a
+/// mechanism, if necessary, to avoid messages piling up.
+///
+/// The returned `SyncSender` can be used to send messages that are processed by
+/// the returned `Receiver`. The `SyncSender` can be cloned to send messages
+/// from multiple sources simultaneously.
+pub fn sync_channel<T>(bound: usize, handle: &Handle) -> io::Result<(SyncSender<T>, Receiver<T>)>
+    where T: Send + 'static,
+{
+    let (tx, rx) = channel::sync_channel(bound);
+    let rx = try!(PollEvented::new(rx, handle));
+    Ok((SyncSender { tx: tx }, Receiver { rx: rx }))
+}
+
 impl<T> Sender<T> {
     /// Sends a message to the corresponding receiver of this sender.
     ///
@@ -85,6 +117,59 @@ impl<T> Sender<T> {
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Sender<T> {
         Sender { tx: self.tx.clone() }
+    }
+}
+
+impl<T> SyncSender<T> {
+    /// Sends a message to the corresponding receiver of this sender.
+    ///
+    /// This function will block until space in the internal buffer becomes available.
+    /// Otherwise, the message provided will be enqueued on the channel immediately, and
+    /// this function will return immediately.
+    ///
+    /// If an I/O error happens while sending the message, or if the receiver
+    /// has gone away, then an error will be returned. Note that I/O errors here
+    /// are generally quite abnormal.
+    pub fn send(&self, t: T) -> io::Result<()> {
+        self.tx.send(t).map_err(|e| {
+            match e {
+                channel::SendError::Io(e) => e,
+                channel::SendError::Disconnected(_) => {
+                    io::Error::new(io::ErrorKind::Other,
+                                   "channel has been disconnected")
+                }
+            }
+        })
+    }
+
+    /// Sends a message to the corresponding receiver of this sender.
+    ///
+    /// The message provided will be enqueued on the channel immediately, and
+    /// this function will return immediately.
+    ///
+    /// If an I/O error happens while sending the message, or if the receiver
+    /// has gone away, or the buffer is full, then an error will be returned.
+    /// Note that I/O errors here are generally quite abnormal.
+    pub fn try_send(&self, t: T) -> io::Result<()> {
+        self.tx.try_send(t).map_err(|e| {
+            match e {
+                channel::TrySendError::Io(e) => e,
+                channel::TrySendError::Disconnected(_) => {
+                    io::Error::new(io::ErrorKind::Other,
+                                   "channel has been disconnected")
+                }
+                channel::TrySendError::Full(_) => {
+                    io::Error::new(io::ErrorKind::Other,
+                                   "channel full")
+                }
+            }
+        })
+    }
+}
+
+impl<T> Clone for SyncSender<T> {
+    fn clone(&self) -> SyncSender<T> {
+        SyncSender { tx: self.tx.clone() }
     }
 }
 
